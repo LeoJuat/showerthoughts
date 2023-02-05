e2d7a278f9aaa91e55984de4fa6a45c19560603a
leojuat <leonellejuat@gmail.com>
2023-02-04 18:28:01 -0800

    fixed logo in nav bar

--- src/Components/NavBarLogin.js
+++ src/Components/NavBarLogin.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../imgs/showerthoughts.png";
 
 const NavBarLogin = () => {
   return (
     <nav className="w-5/6 py-10 mx-auto background">
       <ul className="flex items-center text-[#E85A4F] justify-between">
-        <Link
-          to="/"
-          className="text-4xl font-extrabold tracking-wide duration-200 hover:scale-110"
-        >
-          Logo
+        <Link to="/" className="w-1/4 duration-200 lg:w-1/12 hover:scale-110">
+          <img className="-translate-x-6" src={logo} alt="logo" />
         </Link>
         <div className="flex items-center gap-16">
           <Link
